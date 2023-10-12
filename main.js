15f004104857214c04c269451a805af042b383bd
Wagner Alves <angelewagner@hotmail.com>
2023-10-12 08:38:02 -0300

    commiti inicial

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('#cpf').mask('000.000.000-00')
+
+    $('#cep').mask('00.000-00')
+
+    $('#telefone').mask('(00) 0000-0000')
+
+    $('form').validate({
+      rules: {
+        nome: {
+          required: true
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        telefone: {
+          required: true
+        },
+        cpf: {
+          required: true,
+          cpf: true
+        },
+        cep: {
+          required: true,
+          cep: true
+        }
+      },
+      messages: {
+        nome: 'Por favor, insira o seu nome'
+      },
+      submitHandler: function(form) {
+        console.log(form)
+      },
+      invalidHandler: function(evento, validador) {
+        let camposIncorretos = validador.numberOfInvalids();
+        if(camposIncorretos) {
+          alert(`Existem ${camposIncorretos} campos incorretos`)
+        }
+      }
+})
+})
