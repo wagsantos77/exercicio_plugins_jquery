989b0850f3c1f09fa1f8b2564a5eb0d12d49ed7a
Wagner Alves <angelewagner@hotmail.com>
2023-10-15 10:33:48 -0300

    acerto cep

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#cpf').mask('000.000.000-00')
 
-    $('#cep').mask('00.000-00')
+    $('#cep').mask('00.000-000')
 
     $('#telefone').mask('(00) 0000-0000')
 
